99cc7dc5a66a070ae3f96b405fbe38356df39a9e
ChrisMa <405120508@qq.com>
2017-12-15 13:50:06 +0800

    modify: Task0.py print First record of texts & Last record of calls

--- Task1.py
+++ Task1.py
@@ -11,9 +11,25 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+diff_num = []
+
+def check_num(num):
+    if num not in diff_num:
+        diff_num.append(num)
+
+for text in texts:
+    check_num(text[0])
+    check_num(text[1])
+
+for call in calls:
+    check_num(call[0])
+    check_num(call[1])
+
+print("There are {} different telephone numbers in the records.".format(len(diff_num)))
 
 """
 任务1：
 短信和通话记录中一共有多少电话号码？每个号码只统计一次。
 输出信息：
-"There are <count> different telephone numbers in the records.""""
+"There are <count> different telephone numbers in the records."
+"""
